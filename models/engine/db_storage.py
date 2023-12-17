e17f66a057de79e1f5bf5f2c7ff3850fa812d6cc
Mariana Echeto <echetomariana@gmail.com>
2023-12-17 19:59:08 -0300

    db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,6 +69,5 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        """ Calls remove() on the private session attribute """
-        self.__session.remove()
-        self.__init__()
+        """ Calls close() on the private session attribute """
+        self.__session.close()
