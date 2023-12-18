064dadfb0032f9df74dd5d9a4936b6888a5a2e68
Mariana Echeto <echetomariana@gmail.com>
2023-12-17 21:40:52 -0300

    FINAL FIX

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 from models.base_model import Base
-from models import classes
 
 
 class DBStorage:
@@ -28,24 +27,25 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """ Queries all objects in the current session """
-
-        objects = {}
+        """Queries current database session for all objects of a given class"""
+        obj_dict = {}
         if cls:
-            query_result = self.__session.query(classes[cls]).all()
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = f'{obj.__class__.__name__}.{obj.id}'
+                obj_dict[key] = obj
         else:
-            for cls in classes.values():
-                query_result = self.__session.query(cls).all()
-                for obj in query_result:
-                    key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-                    objects[key] = obj
-
-        return objects
+            for class_name in classes:
+                objs = self.__session.query(classes[class_name]).all()
+                for obj in objs:
+                    key = f'{obj.__class__.__name__}.{obj.id}'
+                    obj_dict[key] = obj
+        return obj_dict
 
     def new(self, obj):
         """ Adds the object to the current session """
-        if obj not in self.__session:
-            self.__session.add(obj)
+        # if obj not in self.__session:
+        self.__session.add(obj)
 
     def save(self):
         """ Commits all changes of the current session """
@@ -64,9 +64,9 @@ class DBStorage:
     def reload(self):
         """ Creates all tables in the database and creates a session """
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(
-            bind=self.__engine, expire_on_commit=False))
+        # self.__session = scoped_session(sessionmaker(
+        #     bind=self.__engine, expire_on_commit=False))
 
     def close(self):
         """ Calls close() on the private session attribute """
-        self.__session.close()
+        self.__session.remove()
